e00a75f0e952937684dcbaf7ed30536fe756e643
Jose Eduardo <josee.almeida@outlook.com>
2019-05-30 20:26:41 -0300

    services2

--- Projeto_Cafe/src/app/services/autentication-service.service.ts
+++ Projeto_Cafe/src/app/services/autentication-service.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import * as flogin from 'firebase/app';
+import { reject } from 'q';
+import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,42 @@ import { Injectable } from '@angular/core';
 export class AutenticationServiceService {
 
   constructor() { }
+
+  cadastroUsuario(user){
+    return new Promise<any>(( resolve , reject) => {
+      flogin.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then(
+          res => resolve(res),
+          err => reject(err)
+        )
+    });
+  }
+
+  loginUsuario(user){
+    return new Promise<any> ( (resolve, reject) => {
+      flogin.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(
+          res => resolve (res),
+          err => reject(err)
+        )
+    } );
+  }
+
+  logoutUsuario(){
+    return new Promise<any>( (resolve, reject) => {
+      if(flogin.auth().currentUser){
+        flogin.auth().signOut()
+          .then( () => {
+            console.log("log out");
+            resolve();
+          } )
+          .catch( (error) => { reject(); } );
+      }
+    });
+  }
+
+  detalhesUsuario(){
+    return flogin.auth().currentUser;
+  }
+
 }
